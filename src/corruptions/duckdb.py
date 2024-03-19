d7be6fa06442ac14da2cef6743600b6c99e81f33
Coen <coen.mol@student.uva.nl>
2024-03-19 23:47:39 +0100

    feat: added duckdb corruption

--- src/corruptions/duckdb.py
+++ src/corruptions/duckdb.py
@@ -1,4 +1,19 @@
 from ..jenga.basis import TabularCorruption 
-#
-# class DuckDBCorruption(TabularCorruption):
-#     def
+import duckdb
+from pandas import DataFrame
+import pandas.api.types as ptypes
+class DuckDBCorruptionBrokenCharacters(TabularCorruption):
+    def transform(self, data : DataFrame) -> DataFrame:
+        assert self.column in data.columns
+        return duckdb.sql(
+            f"""
+            SELECT 
+                * REPLACE(
+                    CASE WHEN random() < {self.fraction} THEN translate({self.column}, 'aAeEoOuU', 'áÁéÉớỚúÚ')  ELSE {self.column} END AS {self.column}
+                )
+            FROM data
+            ORDER BY {self.column}
+            """
+        ).to_df()
+
+
