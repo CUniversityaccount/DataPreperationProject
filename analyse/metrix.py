b014e2039197740a1c9345872ecd093889370d98
SydWingss <sydwings@outlook.com>
2024-04-05 13:07:49 +0200

    update metrix

--- analyse/metrix.py
+++ analyse/metrix.py
@@ -5,8 +5,7 @@ from transformers import AutoModel, AutoTokenizer
 import torch
 #Ensure that the corresponding model is installed
 #python -m spacy download en_core_web_md
-word_vector_similarity= spacy.load('en_core_web_md')  # 确保安装了相应的模型
-
+word_vector_similarity= spacy.load('en_core_web_md') 
 
 sentiment_pipeline = pipeline("sentiment-analysis")
 nli_model = pipeline('text-classification', model='facebook/bart-large-mnli')
@@ -30,6 +29,9 @@ def get_similarity(text1, text2):
 
 #calculate emotional similarity. return 1 if the two texts have the same emotion, otherwise return 0
 def get_emotional_similarity(text1,text2):
+    text1 = text1[:512]
+    text2 = text2[:512]
+
     result1 = sentiment_pipeline(text1)
     result2 = sentiment_pipeline(text2)
     if result1[0]['label'] == result2[0]['label']:
@@ -39,13 +41,19 @@ def get_emotional_similarity(text1,text2):
 
 
 #calculate the cosine similarity between two texts
-def get_cosine_similarity(text1,text2):
-    inputs = tokenizer([text1, text2], padding=True, return_tensors="pt")
+def get_cosine_similarity(text1, text2):
+    inputs1 = tokenizer(text1, padding='max_length', truncation=True, max_length=512, return_tensors="pt")
+    inputs2 = tokenizer(text2, padding='max_length', truncation=True, max_length=512, return_tensors="pt")
+    
     with torch.no_grad():
-        outputs = bert_model(**inputs)
-    embeddings = outputs.last_hidden_state.mean(dim=1)
+        outputs1 = bert_model(**inputs1)
+        outputs2 = bert_model(**inputs2)
+    
+    embeddings1 = outputs1.last_hidden_state.mean(dim=1)
+    embeddings2 = outputs2.last_hidden_state.mean(dim=1)
+    
     cosine_similarity = torch.nn.CosineSimilarity(dim=0)
-    similarity = cosine_similarity(embeddings[0], embeddings[1]).item()
+    similarity = cosine_similarity(embeddings1[0], embeddings2[0]).item()
     return similarity
 
 #calculate the logical relationship between two texts
